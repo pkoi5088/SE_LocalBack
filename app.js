ac2b86ae47d202c9862cbdc2c57411bc20c5dccb
pkoi5088 <pkoi5088@gmail.com>
2020-11-10 14:35:13 +0900

    login register mysql

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const models = require("./models/index");
+const express = require('express');
+const app = express();
+const port = 3000;
+const session = require('express-session');
 
 var bodyParser = require('body-parser');
 var mainRouter = require('./router/main');
 var loginRouter = require('./router/login');
 var registerRouter = require('./router/register');
+
 
 //뷰엔진 설정
 app.set('view engine','ejs');
@@ -16,18 +17,16 @@ app.use(express.static('public'));
 //body-parser설정
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitalized: true
+}));
 
 //라우터 설정
 app.use('/', mainRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
-
-models.sequelize.sync().then( () => {
-    console.log("DB 연결 성공");
-}).catch(err => {
-    console.log("DB 연결 실패");
-    console.log(err);
-});
 
 app.listen(port, () => {
     console.log("Express server has started on port 3000");
--- router/register.js
+++ router/register.js
@@ -1,30 +1,37 @@
 var express = require('express'); 
 var router = express.Router();
-var models = require("../models");
+var db_config = require('../config/database');
+var conn = db_config.init();
+
+db_config.connect(conn);
  
 router.get('/', function(req, res, next) {
     res.render('register');
 });
 
 router.post('/', function(req, res, next) {
-    let body = req.body;
-    
-    if(body.email==""||body.password==""||body.username==""){
-        res.redirect("/register");
-    }
+    var body = req.body;
+    var sql = 'SELECT * from user WHERE userID = ?';
 
-    models.User.create({
-        userID: body.email,
-        userPW: body.password,
-        userName: body.username
-    }).then( result => {
-        alert("회원가입 완료!");
-        console.log("회원가입 완료");
-        res.redirect("/");
-    }).catch(err => {
-        alert("회원가입 실패!");
-        console.log("회원가입 실패");
-    })
+    conn.query(sql,body.email,function(err, result){
+        if(err){
+            console.log('err: ' + err);
+        }else{
+            if(result.length !== 0){
+                //유저가 이미 존재한다.
+            }else{
+                var insertSql = 'INSERT INTO user VALUES(?,?,?)';
+                var params = [body.email,body.password,body.username];
+                conn.query(insertSql, params, function(err){
+                    if(err){
+                        console.log('err: ' + err);
+                    }else{
+                        res.redirect('/');
+                    }
+                });
+            }
+        }
+    });
 });
 
 module.exports = router;
